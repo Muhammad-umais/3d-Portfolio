b6ca16841218c7e7abe46eb3a178447c8bb38fed
Muhammad Umais <umaisimran96@gmail.com>
2026-01-07 17:03:26 +0500

    Need some changes almost complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 import NavBar from "./components/NavBar.jsx";
-
+import LogoShowcase from "./sections/LogoSection.jsx";
+import FeatureCards from "./sections/FeatureCards.jsx";
+import Experience from "./sections/Experience.jsx";
+import TechStack  from "./sections/TechStake.jsx";
+import Testimonials from "./sections/Testimonials.jsx";
+import Contact from "./sections/Contact.jsx";
+import Footer from "./sections/Footer.jsx";
 const App = () => (
     <>
         <NavBar />
         <Hero />
         <ShowcaseSection />
-        {/*<LogoShowcase />*/}
-        {/*<FeatureCards />*/}
-        {/*<Experience />*/}
-        {/*<TechStack />*/}
-        {/*<Testimonials />*/}
-        {/*<Contact />*/}
-        {/*<Footer />*/}
+        <LogoShowcase />
+        <FeatureCards />
+        <Experience />
+        <TechStack />
+        <Testimonials />
+        <Contact />
+        <Footer />
     </>
 );
 
