c520e558097372705352c3663250ba48041807b7
Muhammad Umais <umaisimran96@gmail.com>
2026-01-07 17:28:20 +0500

    another error resolved

--- src/components/contact/ContactExperience.jsx
+++ src/components/contact/ContactExperience.jsx
@@ -1,7 +1,7 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 
-import Computer from "./Computer";
+import Computer from "./computer.jsx";
 
 const ContactExperience = () => {
     return (
