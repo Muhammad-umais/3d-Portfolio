db28faa22604c9f91bd9f54fcbd7458faba702db
Muhammad Umais <umaisimran96@gmail.com>
2026-01-08 12:08:31 +0500

    Correction

--- src/constants/index.js
+++ src/constants/index.js
@@ -29,21 +29,15 @@ const words = [
 ];
 
 const counterItems = [
-    { value: 15, suffix: "+", label: "Years of Experience" },
-    { value: 200, suffix: "+", label: "Satisfied Clients" },
-    { value: 108, suffix: "+", label: "Completed Projects" },
+    { value: 4, suffix: "+", label: "Years of Experience" },
+    { value: 60, suffix: "+", label: "Satisfied Clients" },
+    { value: 80, suffix: "+", label: "Completed Projects" },
     { value: 90, suffix: "%", label: "Client Retention Rate" },
 ];
 
 const logoIconsList = [
     {
         imgPath: "/images/logos/company-logo-1.png",
-    },
-    {
-        imgPath: "/images/logos/company-logo-2.png",
-    },
-    {
-        imgPath: "/images/logos/company-logo-3.png",
     },
     {
         imgPath: "/images/logos/company-logo-4.png",
@@ -62,12 +56,6 @@ const logoIconsList = [
     },
     {
         imgPath: "/images/logos/company-logo-9.png",
-    },
-    {
-        imgPath: "/images/logos/company-logo-10.png",
-    },
-    {
-        imgPath: "/images/logos/company-logo-11.png",
     },
 ];
 
